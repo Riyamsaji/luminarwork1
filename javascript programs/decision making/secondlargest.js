d0fedd04381052dca022827afda0f4b59d72917a
riya <riyamanalil@gmail.com>
2021-06-14 07:15:31 +0530

    assignments

---
+++ javascript programs/decision making/secondlargest.js
@@ -0,0 +1,10 @@
+var num1=7,num2=8,num3=9;
+if((num1>num2)&(num1<num3)){
+    console.log(`${num1} is the secondlargest`);
+}
+else if((num2>num1)&(num2<num3)){
+    console.log(`${num2} is the secondlargest`);
+}
+else if((num1<num2)&(num2>num3)){
+    console.log(`${num3}is the secondlargest`);
+}
