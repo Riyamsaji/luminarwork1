e173319da5dc903a9bd22b8af8cead0479b16454
riya <riyamanalil@gmail.com>
2021-06-17 06:56:47 +0530

    modified

--- javascript programs/decision making/secondlargest.js
+++ javascript programs/decision making/secondlargest.js
@@ -1,2 +1,28 @@
 var num1=5,num2=7,num3=2;
-if(nu)
+if(num1>num2 & num1>num3){
+   
+    if(num2>num3){
+        console.log(num2);
+    }
+    else{
+        console.log(num3);
+    }
+}
+else if(num2>num1 & num2>num3){
+  
+    if(num1>num3){
+console.log(num1);
+    }
+    else{
+        console.log(num3);
+    }
+}
+else if(num3>num2 & num3>num1){
+  
+    if(num2>num1){
+        console.log(num2);
+    }
+    else{
+        console.log(num1);
+    }
+}
